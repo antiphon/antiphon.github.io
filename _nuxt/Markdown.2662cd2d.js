fe29167be6b46a027c1c4aad7048af62a352b0aa
Tuomas Rajala <tuomas.rajala@iki.fi>
2023-11-04 09:57:50 +0200

    nuxt 3 v

---
+++ _nuxt/Markdown.2662cd2d.js
@@ -0,0 +1,1 @@
+import r from"./ContentSlot.06586a41.js";import{j as o,k as u,G as f,z as c}from"./entry.3674550c.js";const i=o({name:"Markdown",extends:r,setup(t){const{parent:e}=c(),{between:n,default:a}=u(),s=f(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:s,between:n,parent:e}}});export{i as default};
---
+++ _nuxt/ProseCode.a506bdc8.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./ProseCode.vue.e71bdcc1.js";import"./entry.3674550c.js";export{m as default};
